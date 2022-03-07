80be723738c2c519ff667588df46be74c1034b86
* <mejiamicoltalol@gmail.com>
2022-03-07 10:50:29 -0500

    hola que tal? como estas?

---
+++ suma.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+	float x,y,z;
+	cout<<"Ingrese dos numeros separados de espacio x y:"<<endl;
+	cin>>x>>y;
+	z=x+y;
+	cout<<"El resultado es:"<<endl;
+	cout<<z<<endl;
+	return 0;
+}
